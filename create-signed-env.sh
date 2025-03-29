8e4329922812dc51aa6d4e4915231105589710f4
gianogli <gianogli@tiscali.it>
2025-03-29 22:11:28 +0100

    Remove the code to check the parameters

--- create-signed-env.sh
+++ create-signed-env.sh
@@ -26,14 +26,14 @@ echo "Using Subject Line:"
 echo "$subject"
 
 # Prompt the user to verify if the subject line is correct
-read -p "Is the subject line correct? (y/n): " confirmation
+#read -p "Is the subject line correct? (y/n): " confirmation
 
 # Check the user's response
-if [[ $confirmation != "y" && $confirmation != "Y" ]]; then
-    echo "Exiting without changes."
-    exit 1
-fi
-clear
+#if [[ $confirmation != "y" && $confirmation != "Y" ]]; then
+#    echo "Exiting without changes."
+#    exit 1
+#fi
+#clear
 
 
 # Create Key
