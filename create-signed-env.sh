a5f9e531d5d7077b197ebd96cc64bd06af392a20
gianogli <gianogli@tiscali.it>
2025-03-30 00:24:48 +0100

    Set my fixed parameters for the subject

--- create-signed-env.sh
+++ create-signed-env.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 # Prompt the user for each part of the subject line
-read -p "Enter country code 'US' (C): " country
-read -p "Enter state or province name 'California' (ST): " state
-read -p "Enter locality 'Los Angeles' (L): " locality
-read -p "Enter organization name 'crDroid' (O): " organization
-read -p "Enter organizational unit 'crDroid' (OU): " organizational_unit
-read -p "Enter common name 'crdroid' (CN): " common_name
-read -p "Enter email address 'android@android.com' (emailAddress): " email
+#read -p "Enter country code 'US' (C): " country
+#read -p "Enter state or province name 'California' (ST): " state
+#read -p "Enter locality 'Los Angeles' (L): " locality
+#read -p "Enter organization name 'crDroid' (O): " organization
+#read -p "Enter organizational unit 'crDroid' (OU): " organizational_unit
+#read -p "Enter common name 'crdroid' (CN): " common_name
+#read -p "Enter email address 'android@android.com' (emailAddress): " email
+
+# Set my fixed parameters for the subject
+country="IT"
+state="Italy"
+locality="Biella"
+organization="crDroid-Unofficial"
+organizational_unit="crDroid-Unofficial"
+common_name="gianogli"
+email="gianogli@tiscali.it"
 
 # Construct the subject line
 subject="/C=${country}/ST=${state}/L=${locality}/O=${organization}/OU=${organizational_unit}/CN=${common_name}/emailAddress=${email}"
